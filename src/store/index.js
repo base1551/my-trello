85bed5bd71ce0498e99516cecf8cb01a41b57b8d
base1551 <baseball.1.kyoto@gmail.com>
2021-01-23 23:11:14 +0900

    カード削除

--- src/store/index.js
+++ src/store/index.js
@@ -7,49 +7,55 @@ const savedLists = localStorage.getItem('trello-lists')
 
 
 const store = new Vuex.Store({
-  state: {
-    lists: savedLists ? JSON.parse(savedLists) : [{
-        title: 'Backlog',
-        cards: [{
-            body: 'English'
-          },
-          {
-            body: 'Mathematics'
-          },
-        ]
-      },
-      {
-        title: 'Todo',
-        cards: [{
-          body: 'Science'
-        }]
-      },
-      {
-        title: 'Doing',
-        cards: []
-      }
-    ],
-    // ★ここまで③編集
-  },
-  mutations: {
-    addlist(state, payload) {
-      state.lists.push({
-        title: payload.title,
-        cards: []
-      })
+    state: {
+        lists: savedLists ? JSON.parse(savedLists) : [{
+            title: 'Backlog',
+            cards: [{
+                body: 'English'
+            },
+                {
+                    body: 'Mathematics'
+                },
+            ]
+        },
+            {
+                title: 'Todo',
+                cards: [{
+                    body: 'Science'
+                }]
+            },
+            {
+                title: 'Doing',
+                cards: []
+            }
+        ],
     },
-  },
-  actions: {
-    addlist(context, payload) {
-      context.commit('addlist', payload)
+    mutations: {
+        addlist(state, payload) {
+            state.lists.push({
+                title: payload.title,
+                cards: []
+            })
+        },
+        removelist(state, payload) {
+            state.lists.splice(payload.listIndex, 1)
+        }
     },
-  },
-  getters: {}
+    //mutationsで作成したメソッドをcommitで実行
+    actions: {
+        addlist(context, payload) {
+            context.commit('addlist', payload)
+        },
+        removelist(context, payload) {
+            context.commit('removelist', payload)
+        }
+    },
+    getters: {}
 })
 
 // ★ここから追記
 store.subscribe((mutation, state) => {
-  localStorage.setItem('trello-lists', JSON.stringify(state.lists))
+    localStorage.setItem('trello-lists', JSON.stringify(state.lists))
 })
 
 export default store
